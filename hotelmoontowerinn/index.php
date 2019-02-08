ffd953dfa22fd4373b695447f326e7b725264180
ashimdahal <44808756+ashimdahal@users.noreply.github.com>
2019-02-08 19:10:01 +0545

    Add files via upload

---
+++ hotelmoontowerinn/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link rel="shortcut icon" href="">
+  <link rel="stylesheet" href="bootstrap/bb.css">
+<script src="jq/jq.js">
+
+</script>
+  <body>
+
+  </body>
+</html>
