ca8c1aa4ea649ef5f7fbae0b9f296ae399052686
Dewanik <kdewanik@gmail.com>
2019-02-09 08:16:28 +0545

    First Commit

--- hotelmoontowerinn/index.php
+++ hotelmoontowerinn/index.php
@@ -1,15 +1,78 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <link rel="shortcut icon" href="">
-  <link rel="stylesheet" href="bootstrap/bb.css">
-<script src="jq/jq.js">
+<html>
+<head>
+	<title>Moon-Tower Inn</title>
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css?family=Abel|Raleway|Staatliches" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<nav>
+		
+<div class='jumbotron'>
+	<img src="hotel_images/Hotel_MoonTower_Inn.png" >
+<ul class='options'>
+			<li>
+			Home
+		</li>
+			<li>
+			Rooms
+		</li>
+			<li>
+			Travels & Tourism
+		</li>
+			<li>
+			Food Menu 
+		</li>
+		<li>
+			About Us
+		</li>
+		</ul>
+<h2 class="caption">Searching For Something Best in Birtamode ?</h2>
+<div class='quick_book_acess'>
+	<h4 class='caption_2'>Well , You are in a Right Place ! <br>
+	Hotel Moon Tower Inn</h4>
+
+<div class='quick_book'>
+	<h4>Book Us Now !! </h4>
+</div>
+
+</div>
+
+</div>
+
+</nav>
+
+<section>
+	
+</section>
+
+
+<script>
+//Image Cordinate SLIDE IF NEEDED
+
+$('.jumbotron').mousemove(function(e){
+
+var mpagex = e.pageX
+var mpagey = e.pageY
+
+
+if(mpagex > $('.jumbotron').width()/2){
+	$('.jumbotron').css('background-position','right')
+}else if(mpagex < $('.jumbotron').width()/2){
+	$('.jumbotron').css('background-position','left')
+}
+
+});
+
+
+
+
 
 </script>
-  <body>
-
-  </body>
+</body>
 </html>
