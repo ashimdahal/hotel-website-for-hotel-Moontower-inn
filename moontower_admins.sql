20c1229607185b8721378363f3a19a78cba7a6e0
Dewanik <kdewanik@gmail.com>
2019-02-11 20:33:30 +0545

    Sql Database Update

--- moontower_admins.sql
+++ moontower_admins.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2019 at 01:19 PM
+-- Generation Time: Feb 11, 2019 at 03:47 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -43,8 +43,59 @@ CREATE TABLE `admins_users` (
 
 INSERT INTO `admins_users` (`id`, `uname`, `password`, `company_post`, `added_date`, `last_login`) VALUES
 (1, 'Chandra', 'moontowerinn_22', 'Owner', '2019-02-09', '2019-02-09 05:11:13'),
-(3, 'a', 'a', 'a', '2019-02-09', '0000-00-00 00:00:00'),
-(4, 'a', 'aa', 'a', '2019-02-09', '0000-00-00 00:00:00');
+(8, 'dewanik', 'dewanik8', 'Developer', '2019-02-10', '2019-02-10 08:17:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `available_rooms_criteria`
+--
+
+CREATE TABLE `available_rooms_criteria` (
+  `id` int(11) NOT NULL,
+  `checkin_date` text NOT NULL,
+  `checkout_date` text NOT NULL,
+  `total_people` int(11) NOT NULL,
+  `added_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `available_rooms_criteria`
+--
+
+INSERT INTO `available_rooms_criteria` (`id`, `checkin_date`, `checkout_date`, `total_people`, `added_date`) VALUES
+(1, '\r\n[\"2019-02-10\",\"2019-02-28\"]', '[\"2019-02-10\",\"2019-03-21\"]', 100, '2019-02-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `room_cat` text NOT NULL,
+  `room_capacity` text NOT NULL,
+  `available_such_rooms` int(11) NOT NULL,
+  `room_checkin` text NOT NULL,
+  `room_checkout` text NOT NULL,
+  `room_images` text NOT NULL,
+  `room_price` int(11) NOT NULL,
+  `extra_offer` text NOT NULL,
+  `promo_code` int(11) NOT NULL,
+  `discount_available` int(11) NOT NULL,
+  `short_description` text NOT NULL,
+  `room_code` text NOT NULL,
+  `best_peice` text NOT NULL,
+  `added_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `room_cat`, `room_capacity`, `available_such_rooms`, `room_checkin`, `room_checkout`, `room_images`, `room_price`, `extra_offer`, `promo_code`, `discount_available`, `short_description`, `room_code`, `best_peice`, `added_date`) VALUES
+(6, 'Super Deluxe Room', '3', 10, '[\"2019-02-10\",\"2019-02-28\"]', '[\"2019-02-10\",\"2019-03-21\"]', '[\"room_files/5c61213e2dd916.40516180_1.jpg\",\"room_files/5c61213e2dd916.40516180_2.wmv\"]', 300, 'Extra Bed @Rs.220', 2222, 12, 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ', '5c61213e2dd916.40516180', 'Yes', '2019-02-11 01:01:14');
 
 --
 -- Indexes for dumped tables
@@ -57,6 +108,18 @@ ALTER TABLE `admins_users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `available_rooms_criteria`
+--
+ALTER TABLE `available_rooms_criteria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -64,7 +127,19 @@ ALTER TABLE `admins_users`
 -- AUTO_INCREMENT for table `admins_users`
 --
 ALTER TABLE `admins_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `available_rooms_criteria`
+--
+ALTER TABLE `available_rooms_criteria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
